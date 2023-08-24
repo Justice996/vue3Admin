7cd3f0a4e49ded98ccc7d4c9b61af886e30f98c8
陈彦文 <chenyanwen9696@xwtec.cn>
2023-08-24 23:14:35 +0800

    '初始化项目'

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {
+  Edit
+} from '@element-plus/icons-vue'
+</script>
+
+<template>
+  <div>
+    <el-button type="success" size="small" :icon="Edit">Default</el-button>
+  </div>
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
